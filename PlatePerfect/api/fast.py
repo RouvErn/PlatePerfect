c1ead9f2534fbddf46fa8b54c79427d2252197e9
rouven ernst <rouvenernst@yahoo.de>
2024-03-11 12:34:32 +0100

    updated the repo, made the api predictions work

--- PlatePerfect/api/fast.py
+++ PlatePerfect/api/fast.py
@@ -17,14 +17,15 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+# http://127.0.0.1:8000/predict?image_paths=https%3A%2F%2Fmedia.istockphoto.com%2Fid%2F1389527253%2Fde%2Ffoto%2Fvegetarische-pizza-%25C3%25BCber-der-ansicht-minimalistisch-auf-blauem-hintergrund.jpg%3Fs%3D1024x1024%26w%3Dis%26k%3D20%26c%3DMZSxI1weYYPWmQi97r4SdxOeaxskEqQO8If5aD5w-WQ%3D
 @app.get("/predict")
-async def predict(image_paths: str):
+async def predict(image_paths: str, serving_size_grams: int):
     """
     Endpoint to predict food items in images and retrieve nutritional information.
-    Expects a comma-separated string of image paths.
+    Expects a comma-separated string of image paths and serving size in grams.
     """
     image_paths_list = image_paths.split(',')
-    predictions_with_calories = predict_with_pipeline_and_calories(image_paths_list)
+    predictions_with_calories = predict_with_pipeline_and_calories(image_paths_list, serving_size_grams)
     return predictions_with_calories
 
 
