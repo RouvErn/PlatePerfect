b41d5b389a0d1e3e60cfae92e086dfde532cead3
msaffan1 <139398711+msaffan1@users.noreply.github.com>
2024-03-11 12:42:15 +0100

    Add files via upload

--- PlatePerfect/app.py
+++ PlatePerfect/app.py
@@ -4,11 +4,22 @@ import os
 import numpy as np
 import tensorflow as tf
 
-model = tf.keras.models.load_model('food_classification_model.h5')
+#model = tf.keras.models.load_model('food_classification_model.h5')
+
+
+st.set_page_config(
+    page_title="Plate Perfect",
+    page_icon="🍲",
+    initial_sidebar_state="expanded",
+)
+
+
+food_detect = open('food_detect_image.png', 'rb').read()
+st.image(food_detect, use_column_width=False, width=750)
 
 
 st.title('Plate Perfect!')
-st.write('Hello and welcome to Plate perfect. Upload your food image below!')
+st.write('Hello and welcome to Plate Perfect. Upload your food image below!')
 
 # File uploader widget
 uploaded_file = st.file_uploader("Choose a food image...", type=["jpg", "jpeg", "png"])
@@ -26,7 +37,11 @@ def predict(image):
     prediction = model.predict(img)
     return prediction
 
+# User input for serving size
+serving_size = st.number_input("Enter the serving size (in grams):", min_value=1, max_value=1000, step=1, value=100)
 
+# Display the serving size chosen by the user
+st.write("Selected Serving Size:", serving_size, "grams")
 
 # Display the uploaded image
 if uploaded_file is not None:
