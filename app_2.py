941f089ab91ce7b35216999996abb826b52f6264
rouven ernst <rouvenernst@yahoo.de>
2024-03-14 15:02:51 +0100

    changed app_2

--- app_2.py
+++ app_2.py
@@ -40,9 +40,7 @@ def main():
             # service_account_info = json.load(open(os.environ['GOOGLE_APPLICATION_CREDENTIALS']))
             #credentials = service_account.Credentials.from_service_account_info(service_account_info)
 
-            credentials = service_account.Credentials.from_service_account_file(
-            '/Users/RouvenErnst/code/RouvErn/gcp/mineral-bonus-411314-3d34a7aa21e6.json'
-            )
+            credentials = service_account.Credentials.from_service_account_file('/Users/RouvenErnst/code/RouvErn/gcp/mineral-bonus-411314-3d34a7aa21e6.json')
 
             client = storage.Client(credentials=credentials, project='plateperfect') #credentials=os.environ['GOOGLE_APPLICATION_CREDENTIALS'],
             bucket = client.get_bucket('plateperfect_public')
